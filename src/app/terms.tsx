56c3b722b7b853f7d99d797c2a8d4a9da8a4b6b0
antoniolemos34-code <antoniolemos34@gmail.com>
2026-01-28 17:04:38 +0000

    Add Terms and Conditions component

---
+++ src/app/terms.tsx
@@ -0,0 +1,10 @@
+export default function Terms() {
+  return (
+    <main style={{ padding: "40px" }}>
+      <h1>Terms & Conditions</h1>
+      <p>
+        By joining the waitlist you agree to receive communications from us.
+      </p>
+    </main>
+  );
+}
